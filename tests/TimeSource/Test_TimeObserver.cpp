01a9b47ab6a3e5d8332087231e8c295df7010c8f
Kevin Matthews <kmatthews@cyberdata.net>
2016-06-28 10:44:31 -0700

    Notify a second observer

--- tests/TimeSource/Test_TimeObserver.cpp
+++ tests/TimeSource/Test_TimeObserver.cpp
@@ -7,15 +7,22 @@ extern "C"
 #include "CppUTest/TestHarness.h"
 
 static bool is_observer_notified;
+static bool is_observer2_notified;
 
 void dummyNotification(TimeObserver observer, SystemTime system_time)
 {
     is_observer_notified = 1;
 }
 
+void dummyNotification2(TimeObserver observer, SystemTime system_time)
+{
+    is_observer2_notified = 1;
+}
+
 TEST_GROUP(TimeObserver)
 {
     TimeObserver observer;
+    TimeObserver observer2;
 
     void setup()
     {
@@ -47,4 +54,14 @@ TEST(TimeObserver, notify_a_single_observer)
     TimeObserver_Notify(observer, system_time);
 
     CHECK_TRUE(is_observer_notified);
+}
+
+TEST(TimeObserver, notify_a_second_observer)
+{
+    observer2 = TimeObserver_Create(dummyNotification2);
+    SystemTime system_time = NULL;
+
+    TimeObserver_Notify(observer2, system_time);
+
+    CHECK_TRUE(is_observer2_notified);
 }
