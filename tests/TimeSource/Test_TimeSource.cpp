13403a6fadc44eb661229d1b63155791f01b64ed
Kevin Matthews <kmatthews@cyberdata.net>
2016-06-29 17:40:20 -0700

    Refactor TimeSource tests: Utilize setup() and teardown()

--- tests/TimeSource/Test_TimeSource.cpp
+++ tests/TimeSource/Test_TimeSource.cpp
@@ -9,21 +9,6 @@ extern "C"
 static bool is_observer_notified;
 static bool is_observer2_notified;
 
-TEST_GROUP(TimeSource)
-{
-    void setup()
-    {
-        TimeSource_Create();
-        is_observer_notified = 0;
-        is_observer2_notified = 0;
-    }
-
-    void teardown()
-    {
-        TimeSource_Destroy();
-    }
-};
-
 static void dummyNotification(TimeObserver observer, SystemTime system_time)
 {
     is_observer_notified = 1;
@@ -34,6 +19,28 @@ static void dummyNotification2(TimeObserver observer, SystemTime system_time)
     is_observer2_notified = 1;
 }
 
+TEST_GROUP(TimeSource)
+{
+    TimeObserver observer;
+    TimeObserver observer2;
+
+    void setup()
+    {
+        observer = TimeObserver_Create(dummyNotification);
+        observer2 = TimeObserver_Create(dummyNotification2);
+        TimeSource_Create();
+        is_observer_notified = 0;
+        is_observer2_notified = 0;
+    }
+
+    void teardown()
+    {
+        TimeObserver_Destroy(observer);
+        TimeObserver_Destroy(observer2);
+        TimeSource_Destroy();
+    }
+};
+
 TEST(TimeSource, observers_not_notified_by_default)
 {
     CHECK_FALSE(is_observer_notified);
@@ -42,39 +49,29 @@ TEST(TimeSource, observers_not_notified_by_default)
 
 TEST(TimeSource, can_register_an_observer)
 {
-    TimeObserver observer = TimeObserver_Create(dummyNotification);
     LONGS_EQUAL( TS_SUCCESS, TimeSource_RegisterMillisecondTickObserver(observer) );
-    TimeObserver_Destroy(observer);
 }
 
 TEST(TimeSource, can_not_register_too_many_observers)
 {
-    TimeObserver observer = TimeObserver_Create(dummyNotification);
     for (size_t i = 0; i < MAX_OBSERVERS; i++)
     {
         LONGS_EQUAL( TS_SUCCESS, TimeSource_RegisterMillisecondTickObserver(observer) );
     }
     LONGS_EQUAL( TS_TOO_MANY_OBSERVERS, TimeSource_RegisterMillisecondTickObserver(observer) );
-
-    TimeObserver_Destroy(observer);
 }
 
 TEST(TimeSource, tick_notifies_an_observer)
 {
-    TimeObserver observer = TimeObserver_Create(dummyNotification);
     TimeSource_RegisterMillisecondTickObserver(observer);   // We're only offering one tick right now.
 
     TimeSource_MillisecondTick();                           // The HW or OS will call this from a timer.
 
     CHECK_TRUE(is_observer_notified);
-
-    TimeObserver_Destroy(observer);
 }
 
 TEST(TimeSource, tick_notifies_several_observers)
 {
-    TimeObserver observer = TimeObserver_Create(dummyNotification);
-    TimeObserver observer2 = TimeObserver_Create(dummyNotification2);
     TimeSource_RegisterMillisecondTickObserver(observer);
     TimeSource_RegisterMillisecondTickObserver(observer2);
 
@@ -82,7 +79,4 @@ TEST(TimeSource, tick_notifies_several_observers)
 
     CHECK_TRUE(is_observer_notified);
     CHECK_TRUE(is_observer2_notified);
-
-    TimeObserver_Destroy(observer);
-    TimeObserver_Destroy(observer2);
 }
